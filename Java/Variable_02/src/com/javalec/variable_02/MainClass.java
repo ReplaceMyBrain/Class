6aa9217bf0a374ca026047524665bb5b5939a51a
Ray-Ari <80452660+Ray-Ari@users.noreply.github.com>
2021-03-30 12:05:42 +0900

    Class

---
+++ Java/Variable_02/src/com/javalec/variable_02/MainClass.java
@@ -0,0 +1,33 @@
+package com.javalec.variable_02;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+	// 변수와 상수
+	//선언
+	String str = "James"; //class 개체다.
+	int num = 10;
+	double numDouble = 20.2;
+	char chr = 'a';
+	boolean bool =true; // false 따옴표를 안쓰는 유일한것들  / 조건문등에 만힝 쓰임.
+	
+	
+			
+	
+	//출력
+	System.out.println(str + ":" + num);
+	System.out.println(num + num);
+	System.out.println((double)num + numDouble); // 실수 출력
+	System.out.println(num +(int)numDouble); // 정수출력
+	System.out.println(chr);
+	System.out.println(bool);
+	
+	
+	
+	
+
+	
+
+	}
+
+}
---
+++ Java/Variable_03/src/com/javalec/variable_03/MainClass.java
@@ -0,0 +1,10 @@
+package com.javalec.variable_03;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
