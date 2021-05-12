b5cfbdefef85b073a5d05826b1d4b443eac30910
Ray-Ari <mener_marine@naver.com>
2021-05-12 16:13:28 +0900

    2021.05.12

--- JSP/MVCBoard/src/main/java/com/jsplec/bbs/command/BCommand.java
+++
@@ -1,8 +0,0 @@
-package com.jsplec.bbs.command;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface BCommand {
-
-	public void execute(HttpServletRequest request, HttpServletRequest response);
-}
