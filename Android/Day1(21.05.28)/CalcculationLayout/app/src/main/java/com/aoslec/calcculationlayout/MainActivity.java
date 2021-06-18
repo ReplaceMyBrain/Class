261968bef230d43ce61d086e467cd2448ad5c394
DoWoo Kim <mener_marine@naver.com>
2021-06-18 22:10:45 +0900

    test

--- Android/Day1(21.05.28)/CalcculationLayout/app/src/main/java/com/aoslec/calcculationlayout/MainActivity.java
+++ Android/Day1(21.05.28)/CalcculationLayout/app/src/main/java/com/aoslec/calcculationlayout/MainActivity.java
@@ -3,12 +3,32 @@ package com.aoslec.calcculationlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView result;
+    Button c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("CalculationLayout");
+
+        result = findViewById(R.id.tv_result);
+        c = findViewById(R.id.btn_c);
+
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.setText("0");
+            }
+        });
     }
+
+
 }
