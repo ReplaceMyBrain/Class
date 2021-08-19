dd010b5ed81f6bf5227ace2b0b28bc2cb0880268
ReplaceMyBrain <mener_marine@naver.com>
2021-08-20 00:32:29 +0900

    08/19

---
+++ Flutter/Day4(21.08.19)/tabbar_calc_ex_app/android/app/src/main/kotlin/com/example/tabbar_calc_ex_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tabbar_calc_ex_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter/Day4(21.08.19)/tabbar_ex_app/android/app/src/main/kotlin/com/example/tabbar_ex_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tabbar_ex_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
